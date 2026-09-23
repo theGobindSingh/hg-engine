e178b77b74d655ecc7a4b21ef94b8ad1f971e47c
theGobindSingh <thesinghgobind@gmail.com>
2026-09-23 21:16:55 +0000

    0.4.26 metronome_wait: Wait/WaitButtonABTime 30 after Metronome reveal (msg 1483), matching Nature Power

--- data/battle_scripts/effects/effect_script_0083_CALL_RANDOM_MOVE.s
+++ data/battle_scripts/effects/effect_script_0083_CALL_RANDOM_MOVE.s
@@ -16,6 +16,8 @@ _000:
 _010:
     // Waggling a finger let it use {1}!
     PrintMessage 1483, TAG_MOVE_MOVE, BATTLER_CATEGORY_ATTACKER, BATTLER_CATEGORY_MSG_TEMP
+    Wait 
+    WaitButtonABTime 30
     GoBackToBeforeMove
     GoToMoveScript FALSE
     End
